9dccc3007435a0737ecda5e4822a4e1d1c34e0c6
Jessica Kerr <jessitron@honeycomb.io>
2023-03-12 15:23:42 -0500

    Another failling attempt
    This one gets farther.
    
    W0312 20:21:23.198751       1 config.go:46] Environment variable configuration is deprecated, switch to the --aws-region flag.
    W0312 20:21:23.199022       1 client_config.go:549] Neither --kubeconfig nor --master was specified.  Using the inClusterConfig.  This might not work.
    -------------------------------------------------------------------------------
    AWS ALB Ingress controller
      Release:    v1.1.7
      Build:      git-8694851d
      Repository: https://github.com/kubernetes-sigs/aws-alb-ingress-controller.git
    -------------------------------------------------------------------------------
    
    F0312 20:21:23.257204       1 main.go:107] no matches for kind Ingress in version extensions/v1beta1
    
    ... in the pod logs

--- terraform/ingress_controller.tf
+++ terraform/ingress_controller.tf
@@ -1,10 +1,33 @@
-module "eks-alb-ingress" {
-  source  = "lablabs/eks-alb-ingress/aws"
-  version = "0.6.0"
-  # insert the 4 required variables here
-  cluster_identity_oidc_issuer = "" # i have no idea what goes here
-  # aws iam list-open-id-connect-providers
-  cluster_identity_oidc_issuer_arn =""
-  cluster_name = local.cluster_name
-  enabled = true # I guess??
+
+data "aws_region" "current" {}
+
+data "aws_eks_cluster" "target" {
+  name = local.cluster_name
+}
+
+data "aws_eks_cluster_auth" "aws_iam_authenticator" {
+  name = data.aws_eks_cluster.target.name
+}
+
+provider "kubernetes" {
+  alias = "eks"
+  host                   = data.aws_eks_cluster.target.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.target.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.aws_iam_authenticator.token
+}
+
+module "alb_ingress_controller" {
+  source  = "iplabs/alb-ingress-controller/kubernetes"
+  version = "3.4.0"
+
+  providers = {
+    kubernetes = kubernetes.eks
+
+  }
+
+  k8s_cluster_type = "eks"
+  k8s_namespace    = "kube-system"
+
+  aws_region_name  = data.aws_region.current.name
+  k8s_cluster_name = data.aws_eks_cluster.target.name
 }
