7347595d06c70e0d886493c6857fda6a0ff9681a
Jessica Kerr <jessitron@honeycomb.io>
2023-03-11 20:23:10 -0600

    cluster name
    I'm working from hashi's eks tutorial except I refuse to blindly clone their repo

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,10 @@ provider "aws" {
   region = "us-west-2"
 }
 
+locals {
+  cluster_name = "otel-demo-qcon-london-23"
+}
+
 resource "aws_instance" "bananapants-on-ec2" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
