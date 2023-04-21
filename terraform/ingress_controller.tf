4515f8fbef103dbe35fbf1ddc5f3461924fce498
Jessica Kerr <jessitron@honeycomb.io>
2023-04-21 11:32:17 -0500

    Try to enable ALB access logs to s3

--- terraform/ingress_controller.tf
+++ terraform/ingress_controller.tf
@@ -5,3 +5,56 @@ module "load_balancer_controller" {
   cluster_identity_oidc_issuer_arn = module.eks.oidc_provider_arn
   cluster_name                     = local.cluster_name
 }
+
+# This bucket is for putting ALB logs into
+resource "aws_s3_bucket" "alb_log_bucket" {
+  bucket = "otel-demo-alb-access-logs"
+
+  tags = {
+    Notes = "debug 502s from the collector endpoint"
+  }
+}
+
+resource "aws_s3_bucket_acl" "alb_log_bucket_policy" {
+  bucket = aws_s3_bucket.alb_log_bucket.id
+  acl = "public-read"
+#   access_control_policy = {
+
+#     /*
+#     {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Principal": {
+#         "AWS": "arn:aws:iam::elb-account-id:root"
+#       },
+#       "Action": "s3:PutObject",
+#       "Resource": "arn:aws:s3:::bucket-name/prefix/AWSLogs/your-aws-account-id/*"
+#     }
+#   ]
+# }
+#     */ 
+#     {
+#     grant {
+#       grantee {
+#         id   = data.aws_canonical_user_id.current.id
+#         type = "CanonicalUser"
+#       }
+#       permission = "READ"
+#     }
+
+#     grant {
+#       grantee {
+#         type = "Group"
+#         uri  = "http://acs.amazonaws.com/groups/s3/LogDelivery"
+#       }
+#       permission = "READ_ACP"
+#     }
+
+#     owner {
+#       id = data.aws_canonical_user_id.current.id
+#     }
+#   }
+#  }
+}
