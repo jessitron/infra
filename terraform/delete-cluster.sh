1b17dc6d7c2e331ee931588f28d9c4c13e7de3e9
Jessica Kerr <jessitron@honeycomb.io>
2023-06-06 17:12:14 -0500

    Span naming

--- terraform/delete-cluster.sh
+++ terraform/delete-cluster.sh
@@ -6,19 +6,26 @@ fi
 
 export OTEL_EXPORTER_OTLP_HEADERS="X-Honeycomb-Team=$HONEYCOMB_API_KEY"
 export OTEL_EXPORTER_OTLP_ENDPOINT="https://api.honeycomb.io"
+export OTEL_SERVICE_NAME="script"
 
-this_program="$*"
-root_span=$(otel-cli span --tp-print --name "this_program"  | grep TRACEPARENT)
+this_program="$0"
+echo "This program: $this_program"
+root_span=$(otel-cli span --tp-print --name "$this_program"  | grep TRACEPARENT)
 echo $root_span
 export $root_span #this will put future commands in here
 
 function in_span {
   local command=$*
   echo "Let's run a span around: <$command>"
-  otel-cli --service "script" --name "spanny boi" --attrs "message=hello" exec "$command"
+  otel-cli --name "$command" --attrs "message=hello" exec "$command"
 }
 
 #
 # Main
 #
-in_span aws eks delete-cluster --name demo
+
+# aws eks list-nodegroups --cluster-name demo
+# in_span aws eks delete-nodegroup --no-cli-pager --nodegroup-name main --cluster-name demo
+
+# aws eks list-clusters
+in_span aws eks delete-cluster --no-cli-pager --name demo
