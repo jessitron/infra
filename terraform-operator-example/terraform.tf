4c0c5698d3ec6a09c2517def9efa4d6a162ccced
Jessica Kerr <jessitron@honeycomb.io>
2023-09-09 14:19:33 -0500

    Copied in the default values file

--- terraform-operator-example/terraform.tf
+++ terraform-operator-example/terraform.tf
@@ -1,7 +1,17 @@
 terraform {
   backend "s3" {
     bucket = "jessitron-infra"
-    key = "infra/terraform-operator-example/terraform.tfstate"
+    key    = "infra/terraform-operator-example/terraform.tfstate"
     region = "us-west-2"
+  }
+
+  resource "helm_release" "cert-manager" {
+    name       = "cert-manager"
+    repository = "jetstack https://charts.jetstack.io"
+    chart      = "jetstack/cert-manager"
+
+    values = [
+      file("${path.module}/cert-manager-values.yaml")
+    ]
   }
 }
