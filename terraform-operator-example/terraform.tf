f45320bd76d36347653ebaa72e096466c2ebaeb8
Jessica Kerr <jessitron@honeycomb.io>
2023-09-09 14:26:31 -0500

    oops, wrong place

--- terraform-operator-example/terraform.tf
+++ terraform-operator-example/terraform.tf
@@ -4,17 +4,4 @@ terraform {
     key    = "infra/terraform-operator-example/terraform.tfstate"
     region = "us-west-2"
   }
-
-  resource "helm_release" "cert-manager" {
-    name       = "cert-manager"
-    repository = "jetstack https://charts.jetstack.io"
-    chart      = "jetstack/cert-manager"
-    version    = "1.12.4"
-    namespace  = "cert-manager"
-    create-namespace = true
-
-    values = [
-      file("${path.module}/cert-manager-values.yaml")
-    ]
-  }
 }
